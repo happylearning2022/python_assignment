88fc3640ade9d535968616643171ced2f00a7350
vjsmart <96751856+happylearning2022@users.noreply.github.com>
2024-02-20 19:23:16 -0500

    Add files via upload

--- prepare_insert_from_pipeDelimited.py
+++ prepare_insert_from_pipeDelimited.py
@@ -9,11 +9,20 @@ original_strings = original_string.split('\n')
 
 # Prepare insert statements
 insert_statements = []
+header_row = original_strings[0].strip('|').split('|')
 for string in original_strings[1:]:
     if string.startswith('+'):
         continue
     values = [value.strip() for value in string.strip('|').split('|')]
-    insert_statement = f"INSERT INTO table_name (column1, column2, column3) VALUES ('{values[0]}', '{values[1]}', '{values[2]}');"
+    # Ensure number of values matches number of columns in header row
+    if len(values) != len(header_row):
+        print("Number of columns doesn't match with header row.")
+        continue
+    # Generate column names dynamically
+    columns = ', '.join([column.strip() for column in header_row])
+    # Generate values dynamically
+    formatted_values = ", ".join([f"'{value}'" for value in values])
+    insert_statement = f"INSERT INTO table_name ({columns}) VALUES ({formatted_values});"
     insert_statements.append(insert_statement)
 
 # Print insert statements
